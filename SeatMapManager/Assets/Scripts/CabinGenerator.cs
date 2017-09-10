d270067e84e44046558c669a3e18b7db5fcb1316
Robert <packleader@dumbdogentertainment.com>
2017-09-10 18:33:37 -0500

    -- updating with attempt to dynamically create a cabin panel;

---
+++ SeatMapManager/Assets/Scripts/CabinGenerator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CabinGenerator : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        // Create canvas
+        var canvasGameObject = new GameObject(
+            "CabinCanvas",
+            new[] { typeof(Canvas) });
+
+        var canvas = canvasGameObject.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+
+        var canvasRectTransform = canvasGameObject.GetComponent<RectTransform>();
+        canvasRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 25f);
+        canvasRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 50f);
+
+        // create panel
+        var panelGameObject = new GameObject(
+            "CabinPanel",
+            new[] { typeof(Canvas), typeof(Image) });
+
+        var panelRectTransform = panelGameObject.GetComponent<RectTransform>();
+        panelRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 25f);
+        panelRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 50f);
+
+        var backgroundSprite = Resources.Load<Sprite>("Sprites/cabin_panel_background");
+        var panelImage = panelGameObject.GetComponent<Image>();
+        panelImage.sprite = backgroundSprite;
+
+        // set parents
+        panelGameObject.transform.SetParent(canvasGameObject.transform);
+        canvasGameObject.transform.SetParent(this.transform);
+        canvasRectTransform.localPosition = Vector3.zero;
+        canvasRectTransform.localScale = new Vector3(0.2f, 0.2f, 1);
+
+        ////this.gameObject.AddComponent<Canvas>();
+        ////var canvas = this.GetComponent<Canvas>();
+        ////canvas.renderMode = RenderMode.WorldSpace;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ SeatMapManager/Assets/Scripts/SeatMapGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatMapGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int NumberOfRows = 0;
+
+    [SerializeField]
+    private int NumberOfSeatsPerSeatbank = 0;
+
+    public GameObject SeatTilePrefab;
+
+    public float AisleOffset = 1f;
+    public float RowSpacing = 1.25f;
+    public float RowOffset = 1f;
+    public float SeatSpacing = 1.25f;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int row = 0; row < this.NumberOfRows; row++)
+        {
+            this.SpawnRow(row);
+        }
+    }
+
+    private void SpawnRow(int rowNumber)
+    {
+        this.SpawnSeatBank(rowNumber, -1);
+        this.SpawnSeatBank(rowNumber, 1);
+    }
+
+    private void SpawnSeatBank(int rowNumber, int direction)
+    {
+        for (int seatIndex = 0; seatIndex < this.NumberOfSeatsPerSeatbank; seatIndex++)
+        {
+            Instantiate(
+                this.SeatTilePrefab,
+                new Vector3((this.RowOffset + rowNumber) * this.RowSpacing, direction * (this.AisleOffset + seatIndex) * this.SeatSpacing, 0),
+                Quaternion.identity,
+                this.transform);
+        }
+    }
+}
